85f2ff2ea06d95cb00a1871cc93df1e85a87bf31
jayceekaycee <jayceekaycee64@gmail.com>
2016-02-29 21:04:26 -0800

    Update nidoranm.asm

--- data/baseStats/nidoranm.asm
+++ data/baseStats/nidoranm.asm
@@ -1,10 +1,10 @@
 NidoranMBaseStats: ; 38742 (e:4742)
 db DEX_NIDORAN_M ; pokedex id
-db 46 ; base hp
-db 57 ; base attack
-db 40 ; base defense
-db 50 ; base speed
-db 40 ; base special
+db 75 ; base hp
+db 75 ; base attack
+db 75 ; base defense
+db 75 ; base speed
+db 75 ; base special
 db POISON ; species type 1
 db POISON ; species type 2
 db 235 ; catch rate
