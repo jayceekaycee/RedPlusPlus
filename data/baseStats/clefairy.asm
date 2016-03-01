44f4b4c3a0b4a5e88f23e7d2c0d539003aa62fcd
jayceekaycee <jayceekaycee64@gmail.com>
2016-02-29 20:48:32 -0800

    Update clefairy.asm

--- data/baseStats/clefairy.asm
+++ data/baseStats/clefairy.asm
@@ -1,13 +1,13 @@
 ClefairyBaseStats: ; 38796 (e:4796)
 db DEX_CLEFAIRY ; pokedex id
-db 70 ; base hp
-db 45 ; base attack
-db 48 ; base defense
-db 35 ; base speed
-db 60 ; base special
+db 95 ; base hp
+db 105; base attack
+db 92; base defense
+db 90 ; base speed
+db 105 ; base special
 db FAIRY ; species type 1
 db FAIRY ; species type 2
-db 150 ; catch rate
+db 255 ; catch rate
 db 68 ; base exp yield
 INCBIN "pic/bmon/clefairy.pic",0,1 ; 55, sprite dimensions
 dw ClefairyPicFront
