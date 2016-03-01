02e9c89db22b8b185a5003bc47b950774fa7d0a6
jayceekaycee <jayceekaycee64@gmail.com>
2016-02-29 20:54:14 -0800

    Update sandslash.asm

--- data/baseStats/sandslash.asm
+++ data/baseStats/sandslash.asm
@@ -1,10 +1,10 @@
 SandslashBaseStats: ; 386d2 (e:46d2)
 db DEX_SANDSLASH ; pokedex id
-db 75 ; base hp
-db 100 ; base attack
-db 110 ; base defense
-db 65 ; base speed
-db 55 ; base special
+db 115 ; base hp
+db 125 ; base attack
+db 135 ; base defense
+db 95; base speed
+db 80 ; base special
 db GROUND ; species type 1
 db GROUND ; species type 2
 db 90 ; catch rate
